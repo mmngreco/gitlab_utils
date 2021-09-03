fc313f04b88227b4235a1e7a5894d518ba6d436f
Maximiliano Greco <mmngreco@gmail.com>
2021-09-03 13:22:38 +0200

    refactor

--- getProjectsByGroup.sh
+++ getProjectsByGroup.sh
@@ -1,23 +1,5 @@
 #!/usr/bin/env bash
 # https://docs.gitlab.com/ce/api/issues.html#list-project-issues
-
-if [ -f .env ]; then
-    source .env
-fi
-
-if [[ ! -v GITLAB_HOST ]]; then
-    echo '
-    The following varibles are required:
-
-        GITLAB_HOST=gitlab.com
-        GITLAB_TOKEN="asdfasdfkasdf"
-
-    You can define them using `export` in your shell configuration or creating
-    a `.env` file, see the README.md for more information.
-    '
-    exit
-fi
-
 group="${1:-253}"
 url="https://$GITLAB_HOST/api/v4/groups/$group/projects?scope=all&per_page=100"
 header="PRIVATE-TOKEN: $GITLAB_TOKEN"
--- src/download_time_command.sh
+++ src/download_time_command.sh
@@ -1,32 +1,12 @@
 #!/usr/bin/env bash
 # https://docs.gitlab.com/ce/api/issues.html#list-project-issues
-# Usage
-#    ./getMilestone.sh "sprint:40"
-
-if [ -f .env ]; then
-    source .env
-fi
-
-if [[ ! -v GITLAB_HOST ]]; then
-    echo '
-    The following varibles are required:
-
-        GITLAB_HOST=gitlab.com
-        GITLAB_TOKEN="asdfasdfkasdf"
-
-    You can define them using `export` in your shell configuration or creating
-    a `.env` file, see the README.md for more information.
-    '
-    exit
-fi
 
 ENDPOINT="https://$GITLAB_HOST/api/v4/projects/324/issues"
 TMPL="curl --header \"PRIVATE-TOKEN: $GITLAB_TOKEN\" \"$ENDPOINT\"\n"
-MILESTONE="${1:-sprint:40}"
+MILESTONE="${args[--milestone]}"
 SEP="\t"
 header="PRIVATE-TOKEN: $GITLAB_TOKEN"
 url="$ENDPOINT?scope=all&milestone=$MILESTONE&per_page=100"
 jq_query=".[] | (.iid | tostring) + \"$SEP\" + .title  +\"$SEP\" + .web_url  + \"$SEP\" + (.time_stats.time_estimate / 28800 | tostring)"
-
 
 curl -S -s --header "$header" "$url" | jq -r "$jq_query"
